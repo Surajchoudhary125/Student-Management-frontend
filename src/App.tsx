2a49235529a5fd7f8beeeb48b919728d30e68154
suraj choudhary <surajchoudhryji@gmail.com>
2026-07-15 00:05:15 +0530

    first commit on frontend

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,14 @@ import React from 'react';
 
 function App() {
   return (
-    <div>
-      <h1>Hello, World this is my frist project in react!</h1>
-    </div>
+    <>
+      <div>
+        <h1>Hello, World this is my frist project in react!</h1>
+      </div>
+      <div>
+        <h1>Hello, World this is my frist project in react!</h1>
+      </div>
+    </>
   );
 }
 
